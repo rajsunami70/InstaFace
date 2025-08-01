6ca44db74ae8d5ebe74d6df2151ed55ae94953a0
rajsunami70 <rajsunami369@gmail.com>
2025-08-02 00:22:08 +0530

    Project

--- server/index.js
+++ server/index.js
@@ -29,7 +29,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://instaface.onrender.com"], credentials: true }));
+
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
